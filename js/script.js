aac3e157ceccb2181aaf39de5598b70f40a1eef2
Court W <103236243+courtwinters@users.noreply.github.com>
2022-06-11 22:14:05 -0500

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = fetch(“https://randomuser.me/api?results=5”);
+    const data = await usersRequest).json();
+
+    getData();
+};
